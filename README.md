91a4a3a34337e5c0a5bce629e145283a0b066159
Coding Account <codingaccount@Johnathans-MacBook-Pro-2.local>
2018-05-08 16:00:59 -0400

    Updated util.py to allow for same directory data path. Added data.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 **In Python 2.7**
 
-*Data read from ../data/stock.csv... Not uploaded however correctly formatted data can be gathered with Python 3 and the Pandas Library.*
+*Data read from ""../data/{stock}.csv" Not uploaded however correctly formatted data can be gathered with Python 3 and the Pandas Library.*
--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def symbol_to_path(symbol, base_dir=os.path.join("..", "data")):
+def symbol_to_path(symbol, base_dir=os.path.join(".", "data")):
     """Return CSV file path given ticker symbol."""
     return os.path.join(base_dir, "{}.csv".format(str(symbol)))
 
